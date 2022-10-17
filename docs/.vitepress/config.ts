6ebde0ca2b2c22a24677e2a0a29ff3657c02662f
tigger <928489257@qq.com>
2022-10-17 09:46:43 +0800

    update config base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import sidebar from "./config/sidebar"
 import nav from "./config/nav"
 export default {
     title: 'huyikai',
-    base:'mySite',
+    base:'/mySite/',
     description: 'huyikaiSite.',
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
