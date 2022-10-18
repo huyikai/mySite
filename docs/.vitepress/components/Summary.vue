118e25ea89622a0e4de7a296ad79dae09bb98715
tigger <928489257@qq.com>
2022-10-18 08:57:24 +0800

    feat:compatible with multilevel

--- docs/.vitepress/components/Summary.vue
+++ docs/.vitepress/components/Summary.vue
@@ -1,7 +1,15 @@
 <template>
   <ul>
     <li v-for="(item, index) in catalog" :key="index">
-      <a :href="normalizeLink(item.link)">{{ item.text }}</a>
+      <template v-if="item.hasOwnProperty('items')">
+        <h3>{{ item.text }}</h3>
+        <ul>
+          <li v-for="(i, n) in item.items" :key="n">
+            <a :href="normalizeLink(i.link)">{{ i.text }}</a>
+          </li>
+        </ul>
+      </template>
+      <a v-else :href="normalizeLink(item.link)">{{ item.text }}</a>
     </li>
   </ul>
 </template>
@@ -10,6 +18,6 @@
 import { normalizeLink } from 'vitepress/dist/client/theme-default/support/utils.js';
 import { useData } from 'vitepress';
 const data = useData();
-const path = data.page.value.relativePath.replace('index.md','')
+const path = data.page.value.relativePath.replace('index.md', '');
 const catalog = data.theme.value.sidebar[`/${path}`][0]['items'];
 </script>
