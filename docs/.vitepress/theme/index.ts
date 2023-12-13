6fcfb02a9e8291e1585d7debdcf5dfca58268686
huyikai <hyk_tiger@foxmail.com>
2023-12-13 15:24:33 +0800

    update module

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,21 +1,21 @@
-import Comment from './components/Comment.vue'
-import Layout from './Theme.vue';
-import 'vitepress/dist/client/theme-default/styles/fonts.css';
 import 'vitepress/dist/client/theme-default/styles/vars.css';
 import 'vitepress/dist/client/theme-default/styles/base.css';
 import 'vitepress/dist/client/theme-default/styles/utils.css';
 import 'vitepress/dist/client/theme-default/styles/components/custom-block.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-code.css';
+import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-sponsor.css';
-import './styles/vars.scss'
-import './styles/style.scss'
-import NotFound from 'vitepress/dist/client/theme-default/NotFound.vue';
+
+import Home from './home.vue';
+import Layout from '@huyikai/vitepress-helper/theme/Theme.vue';
+import VPBadge from 'vitepress/dist/client/theme-default/components/VPBadge.vue';
+
 const theme = {
-    Layout,
-    NotFound,
-    enhanceApp({ app }) {
-        app.component('Comment', Comment)
-    }
+  Layout,
+  enhanceApp: ({ app }: any) => {
+    app.component('Home', Home);
+    app.component('Badge', VPBadge);
+  }
 };
 export default theme;
