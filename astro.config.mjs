affcdcf032706d4de1fe323338d0d468e5a91a3e
huyikai <hyk_tiger@foxmail.com>
2024-04-19 15:49:56 +0800

    refactor: 💡 merge refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { astroExpressiveCode } from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
-    preact(),
+    
     astroExpressiveCode({
       themes: ['material-theme-darker'],
       plugins: [
