a6319079a4a4a175e94c4b174f3288bdf1481642
huyikai <hyk_tiger@foxmail.com>
2024-04-22 11:29:00 +0800

    style: 💄 social styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { astroExpressiveCode } from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
-
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
-    
     astroExpressiveCode({
       themes: ['material-theme-darker'],
       plugins: [
@@ -14,6 +13,7 @@ export default defineConfig({
           name: '@astrojs/starlight'
         }
       ]
-    })
+    }),
+    icon()
   ]
 });
